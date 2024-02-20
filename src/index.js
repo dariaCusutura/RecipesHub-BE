a9990ec81900fcd2f70f3eacb9845bb330cc9867
dariaCusutura <cusuturadaria@gmail.com>
2024-02-20 13:02:31 +0200

    Build get and post APIs

--- src/index.js
+++ src/index.js
@@ -7,8 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {});
-app.get("/recipes", (req, res) => {});
+const recipes = [
+  { id: 1, name: "Bread" },
+  { id: 2, name: "Cake" },
+  { id: 3, name: "Soup" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+app.get("/recipes/:id", (req, res) => {
+  const recipe = recipes.find((r) => r.id === parseInt(req.params.id));
+  if (!recipe)
+    return res.status(404).send("The recipe with the given id was not found");
+  res.send(recipe);
+});
+
+app.post("/recipes", (req, res) => {
+    const recipe = {
+        id: recipes.length + 1,
+        name: req.body.name
+    };
+    recipes.push(recipe);
+    res.send(recipe);
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000...");
