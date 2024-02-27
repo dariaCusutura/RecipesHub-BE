cadf1ee33c76d4270b9312ed69f406914a042415
dariaCusutura <cusuturadaria@gmail.com>
2024-02-27 10:13:15 +0200

    Managing cookie-parser

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import recipes from "./routes/recipes.js";
 import users from "./routes/users.js";
 import auth from "./routes/authRoutes.js";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 
 mongoose
   .connect("mongodb://localhost/Recipe-hub")
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-// app.use("/recipes", recipes);
+app.use("/recipes", recipes);
 app.use("/users", users);
 app.use("/", auth);
 
